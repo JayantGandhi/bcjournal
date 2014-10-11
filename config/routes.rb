0c93f5fcbcfdba81f8bb9acfc92dfbaefcadf6e9
Jayant Gandhi <jayant.gandhi@gmail.com>
2014-10-11 21:23:08 +0200

    admin users with sub super_admin classification

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
