c9abebc8f13817895aa79ee2b636ae5d7100fefd
Jayant Gandhi <jayant.gandhi@gmail.com>
2014-10-11 21:26:06 +0200

    accidently did the opposite form of auth

--- app/models/only_super_admins_authorization.rb
+++ app/models/only_super_admins_authorization.rb
@@ -3,8 +3,8 @@ class OnlySuperAdminsAuthorization < ActiveAdmin::AuthorizationAdapter
   def authorized?(action, subject = nil)
     case subject
     when normalized(AdminUser)
-      if action == :update || action == :destroy
-        user.super_admin == true
+      if user.super_admin?
+        true
       else
         false
       end
