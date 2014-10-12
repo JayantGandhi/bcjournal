c23655ecf918fc02f5e512208ccda04f9e4c0c56
Jayant Gandhi <jayant.gandhi@gmail.com>
2014-10-12 12:21:30 +0200

    custom form for activeadmin posts

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -2,15 +2,12 @@ ActiveAdmin.register Post do
 
   permit_params :title, :by_line, :content
 
-  form do |f|
-    f.inputs "Post", multipart: true do
-      f.input :title
-      f.input :cover_image, as: :file, hint: f.template.image_tag(f.object.cover_image.url(:thumb))
-      f.input :by_line
-      f.input :content, as: :ckeditor
-    end
-    f.actions
+  def edit
+    @post = Post.find_by_id(params[:id])
+    puts @post.title
   end
+
+  form partial: 'form'
 
 
 end
--- app/assets/javascripts/active_admin.js.coffee
+++ app/assets/javascripts/active_admin.js.coffee
@@ -1,1 +1,16 @@
 #= require active_admin/base
+
+# some code for image previews...
+readURL = (input) ->
+  if input.files and input.files[0]
+    reader = new FileReader()
+    reader.onload = (e) ->
+      $("#img_preview").attr "src", e.target.result
+      return
+
+    reader.readAsDataURL input.files[0]
+  return
+$ ->
+  $("#post_cover_image").change ->
+    readURL this
+    return
