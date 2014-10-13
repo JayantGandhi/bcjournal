d429ef5036515c1e96cfb6e49c0dbcdfde597bb6
Jayant Gandhi <jayant.gandhi@gmail.com>
2014-10-13 13:39:28 +0200

    add published flag to posts

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -2,9 +2,15 @@ ActiveAdmin.register Post, as: 'Article' do
 
   permit_params :title, :by_line, :content, :cover_image
 
-  def edit
-    @post = Post.find_by_id(params[:id])
-    puts @post.title
+  index do
+    selectable_column
+    id_column
+    column :title
+    column 'Author(s)', :by_line
+    column :created_at
+    column :updated_at
+    column 'Published?', :published
+    actions
   end
 
   form partial: 'form'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011214102) do
+ActiveRecord::Schema.define(version: 20141013113636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20141011214102) do
     t.string   "cover_image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "published",   default: false
   end
 
 end
