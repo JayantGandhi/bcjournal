ef11451d9d8189f1f5928858956036844c6932a0
Jayant Gandhi <jayant.gandhi@gmail.com>
2014-11-10 20:44:53 +0100

    ckeditor fix for heroku https://github.com/galetahub/ckeditor/issues/307#issuecomment-22186377

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ module Bcjournal
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.enabled = true
+    config.assets.precompile += Ckeditor.assets
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
   end
 end
