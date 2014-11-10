70591ecffb3cd43ffc31f8eaabf812fae8445461
Jayant Gandhi <jayant.gandhi@gmail.com>
2014-11-10 21:14:37 +0100

    using herokus names

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = ENV['AWS_S3_BUCKET']
+  config.aws_bucket = ENV['S3_BUCKET_NAME']
   config.aws_acl    = :public_read
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
