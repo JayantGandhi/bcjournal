cfb3bc5f182c461708dde7d7655d519a8fd1a219
Jayant Gandhi <jayant.gandhi@gmail.com>
2014-11-12 07:54:40 +0100

    slideshow fix

---
+++ config/initializers/formtastic.rb
@@ -0,0 +1,5 @@
+# config/initializers/formtastic.rb
+
+# You can opt-in to Formtastic's use of the HTML5 `required` attribute on `<input>`, `<select>`
+# and `<textarea>` tags by setting this to true (defaults to false).
+Formtastic::FormBuilder.use_required_attribute = true
