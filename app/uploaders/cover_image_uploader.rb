1beb7c9a699955b1b9c8c4ab53e81ac93a56d1ed
Jayant Gandhi <jayant.gandhi@gmail.com>
2014-11-23 15:55:57 +0100

    change cover image thumb size

--- app/uploaders/cover_image_uploader.rb
+++ app/uploaders/cover_image_uploader.rb
@@ -31,7 +31,7 @@ class CoverImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [50, 50]
+    process resize_to_fit: [100, 100]
   end
 
   version :tile_small do
