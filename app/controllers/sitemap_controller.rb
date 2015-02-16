87bf66fdb699be279ea6bb0098080c3f7464f971
Jayant Gandhi <jayant.gandhi@gmail.com>
2015-02-16 19:16:38 +0100

    working sitempa

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,6 +1,6 @@
 class SitemapController < ApplicationController
   def index
-    @static_pages = ['/', '/about', '/contact', '/submissions']
+    @static_pages = ['http://www.saisjournal.org', 'http://www.saisjournal.org/about', 'http://www.saisjournal.org/contact', 'http://www.saisjournal.org/submissions']
     @posts = Post.all
     @issues = Issue.all
     respond_to do |format|
