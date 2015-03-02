887e0afb3a42ed84610b882ddfb0bb58ec59872b
Jayant Gandhi <jayant.gandhi@gmail.com>
2015-03-02 11:14:46 +0100

    precompiled assets

---
+++ public/assets/verticals-cdbf8d2b1f694ed0deba53558f66c4c7.js
@@ -0,0 +1,25 @@
+(function() {
+  $(function() {
+    var $verticalLinks;
+    $verticalLinks = $('.vertical-link');
+    return $verticalLinks.on('click', function(e) {
+      var $vertical, $verticalWrapper, currentURL, url, verticalSlug, weAreHome;
+      weAreHome = $('#homeHeader').length;
+      if (weAreHome) {
+        currentURL = window.location.href;
+        e.preventDefault();
+        $vertical = $(e.target);
+        $verticalWrapper = $vertical.parent();
+        verticalSlug = $vertical.data('vertical');
+        url = '/posts/vertical/' + verticalSlug;
+        $('.pagination').remove();
+        return $.getScript(url, function() {
+          $('.vertical-link').removeClass('active');
+          $('.' + verticalSlug).addClass('active');
+          return history.pushState(null, '', url);
+        });
+      }
+    });
+  });
+
+}).call(this);
