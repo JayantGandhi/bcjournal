a6983d240041a8de7b60cafd7c40b07e66c55619
Jayant Gandhi <jayant.gandhi@gmail.com>
2015-04-20 12:29:53 +0200

    construct byline from authors

--- app/helpers/names_helper.rb
+++ app/helpers/names_helper.rb
@@ -1,3 +1,27 @@
 module NamesHelper
+  def article_by_line(article)
+    number_of_authors = article.authors.length
+    i = 0
+    by_line = ""
 
+    for author in article.authors
+      i += 1
+
+      if number_of_authors == 1
+        by_line = "#{author.name}"
+      else
+        if i != number_of_authors
+          if number_of_authors > 2
+            by_line += "#{author.name},"
+          else
+            by_line = "#{author.name}"
+          end
+        else
+          by_line += " & #{author.name}"
+        end
+      end
+    end
+
+    return by_line
+  end
 end
